e8d387a3ae5aa4d67a419e0dbdfd04447384f50f
DevTreeStudios <122764654+DevTreeStudios@users.noreply.github.com>
2025-06-07 17:54:12 -0400

    Update script.js

--- Contact/script.js
+++ Contact/script.js
@@ -46,3 +46,10 @@ function showStatus(message, success) {
     statusBox.classList.remove("show");
   }, 5000);
 }
+
+window.onload = function() { 
+  var el = document.getElementById('g-recaptcha-response'); 
+  if (el) { 
+    el.setAttribute('required', 'required'); 
+  } 
+}
