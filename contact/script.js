52f830491a7bc64388ce7caa2ff2ca0d8153119b
DevTreeStudios <122764654+DevTreeStudios@users.noreply.github.com>
2025-06-09 19:37:53 -0400

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -56,10 +56,6 @@ window.onload = function () {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  if (localStorage.getItem("essential_cookie_accepted") === "true") {
-    overlay.style.display = "none";
-  }
-  
   const tokenInput = document.getElementById("g-recaptcha-response");
 
   if (!form || !tokenInput) {
